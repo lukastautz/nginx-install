9f16a4ab7465bef628c2533c4bbbc35e61b319f8
Lukas Tautz <104827162+lukastautz@users.noreply.github.com>
2022-05-15 09:25:45 +0200

    Update php-8.0.sh

--- php-8.0.sh
+++ php-8.0.sh
@@ -3,19 +3,26 @@ sudo apt upgrade -y
 sudo apt install -y curl gnupg2 ca-certificates lsb-release
 sudo apt install -y nginx
 sudo systemctl enable nginx
-sudo apt-get install -y ufw
+sudo apt install -y ufw
 sudo ufw allow 'Nginx HTTP'
 sudo ufw allow 'Nginx HTTPS'
-sudo apt-get -y install software-properties-common
-sudo add-apt-repository -y ppa:ondrej/php
-sudo apt-get -y install php8.0-fpm php8.0-common php8.0-gmp php8.0-curl php8.0-intl php8.0-mbstring php8.0-xmlrpc php8.0-gd php8.0-xml php8.0-zip php8.0-soap php8.0-imap
+sudo apt install -y apt-transport-https software-properties-common
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y php8.0
+sudo apt install -y php8.0-bcmath php8.0-bz2 php8.0-common php8.0-curl php8.0-dba php8.0-dev php8.0-enchant php8.0-fpm php8.0-gd php8.0-gmp php8.0-imap php8.0-interbase php8.0-intl php8.0-ldap php8.0-mbstring
+sudo rm -r /etc/php/8.1
+sudo rm -r /etc/php/8.0/apache2
+sudo rm -r /etc/php/8.0/cli
 sudo rm -r /var/www/html
 sudo rm -r /etc/apache2
 sudo chmod 777 -R -v /var
-sudo chmod 777 -R -v /etc/php/8.0/fpm
+sudo chmod -R -v 777 /etc/php/fpm
 sudo chmod 777 -R -v /etc/nginx/sites-available
-sudo apt-get install -y nginx-extras
-sudo apt-get install -y certbot python3-certbot-nginx
+sudo apt install -y nginx-extras
+sudo apt install -y certbot python3-certbot-nginx
 sudo rm /etc/nginx/sites-available/default
 sudo echo "server {" >> /etc/nginx/sites-available/default
 sudo echo "        listen 80 default_server;" >> /etc/nginx/sites-available/default
@@ -52,19 +59,19 @@ sudo echo 'zlib.output_compression = Off' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'implicit_flush = Off' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'unserialize_callback_func =' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'serialize_precision = -1' >> /etc/php/8.0/fpm/php.ini
-sudo echo 'disable_functions = pcntl_alarm,pcntl_fork,pcntl_waitpid,pcntl_wait,pcntl_wifexited,pcntl_wifstopped,pcntl_wifsignaled,pcntl_wifcontinued,pcntl_wexitstatus,pcntl_wtermsig,pcntl_wstopsig,pcntl_signal,pcntl_signal_get_handler,pcntl_signal_dispatch,pcntl_get_last_error,pcntl_strerror,pcntl_sigprocmask,pcntl_sigwaitinfo,pcntl_sigtimedwait,pcntl_exec,pcntl_getpriority,pcntl_setpriority,pcntl_async_signals,pcntl_unshare,' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'disable_functions =' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'disable_classes =' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'zend.enable_gc = On' >> /etc/php/8.0/fpm/php.ini
-sudo echo 'zend.exception_ignore_args = On' >> /etc/php/8.0/fpm/php.ini
-sudo echo 'expose_php = Off' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'zend.exception_ignore_args = Off' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'zend.exception_string_param_max_len = 15' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'expose_php = On' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'max_execution_time = 60' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'max_input_time = 60' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'memory_limit = 1152M' >> /etc/php/8.0/fpm/php.ini
-sudo echo 'error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'error_reporting = E_ALL' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'display_errors = Off' >> /etc/php/8.0/fpm/php.ini
-sudo echo 'display_startup_errors = Off' >> /etc/php/8.0/fpm/php.ini
-sudo echo 'log_errors = On' >> /etc/php/8.0/fpm/php.ini
-sudo echo 'log_errors_max_len = 1024' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'display_startup_errors = On' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'log_errors = Off' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'ignore_repeated_errors = Off' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'ignore_repeated_source = Off' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'report_memleaks = On' >> /etc/php/8.0/fpm/php.ini
@@ -86,6 +93,21 @@ sudo echo 'max_file_uploads = 100' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'allow_url_fopen = On' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'allow_url_include = Off' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'default_socket_timeout = 60' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=bz2' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=curl' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=ffi' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=ftp' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=fileinfo' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=gd' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=gettext' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=gmp' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=intl' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=imap' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=ldap' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=mbstring' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=exif' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=openssl' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'extension=soap' >> /etc/php/8.0/fpm/php.ini
 sudo echo '[CLI Server]' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'cli_server.color = On' >> /etc/php/8.0/fpm/php.ini
 sudo echo '[Date]' >> /etc/php/8.0/fpm/php.ini
@@ -122,7 +144,7 @@ sudo echo 'mysqli.default_pw =' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'mysqli.reconnect = Off' >> /etc/php/8.0/fpm/php.ini
 sudo echo '[mysqlnd]' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'mysqlnd.collect_statistics = On' >> /etc/php/8.0/fpm/php.ini
-sudo echo 'mysqlnd.collect_memory_statistics = Off' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'mysqlnd.collect_memory_statistics = On' >> /etc/php/8.0/fpm/php.ini
 sudo echo '[OCI8]' >> /etc/php/8.0/fpm/php.ini
 sudo echo '[PostgreSQL]' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'pgsql.allow_persistent = On' >> /etc/php/8.0/fpm/php.ini
@@ -147,7 +169,7 @@ sudo echo 'session.cookie_domain =' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'session.cookie_httponly =' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'session.cookie_samesite =' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'session.serialize_handler = php' >> /etc/php/8.0/fpm/php.ini
-sudo echo 'session.gc_probability = 0' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'session.gc_probability = 1' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'session.gc_divisor = 1000' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'session.gc_maxlifetime = 1440' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'session.referer_check =' >> /etc/php/8.0/fpm/php.ini
@@ -158,7 +180,7 @@ sudo echo 'session.sid_length = 26' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'session.trans_sid_tags = "a=href,area=href,frame=src,form="' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'session.sid_bits_per_character = 5' >> /etc/php/8.0/fpm/php.ini
 sudo echo '[Assertion]' >> /etc/php/8.0/fpm/php.ini
-sudo echo 'zend.assertions = -1' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'zend.assertions = 1' >> /etc/php/8.0/fpm/php.ini
 sudo echo '[COM]' >> /etc/php/8.0/fpm/php.ini
 sudo echo '[mbstring]' >> /etc/php/8.0/fpm/php.ini
 sudo echo '[gd]' >> /etc/php/8.0/fpm/php.ini
@@ -175,9 +197,19 @@ sudo echo '[ldap]' >> /etc/php/8.0/fpm/php.ini
 sudo echo 'ldap.max_links = -1' >> /etc/php/8.0/fpm/php.ini
 sudo echo '[dba]' >> /etc/php/8.0/fpm/php.ini
 sudo echo '[opcache]' >> /etc/php/8.0/fpm/php.ini
-sudo echo '[curl]' >> /etc/php/8.0/fpm/php.ini
-sudo echo '[openssl]' >> /etc/php/8.0/fpm/php.ini
-sudo echo '[ffi]' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.enable=1' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.enable_cli=1' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.jit_buffer_size=1024M' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.jit=1255' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.memory_consumption=1024' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.interned_strings_buffer=16' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.max_accelerated_files=1000000' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.use_cwd=1' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.validate_timestamps=1' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.revalidate_freq=2' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.revalidate_path=1' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.save_comments=0' >> /etc/php/8.0/fpm/php.ini
+sudo echo 'opcache.record_warnings=1' >> /etc/php/8.0/fpm/php.ini
 sudo service nginx restart
 sudo service php8.0-fpm restart
 echo "You can open the standard page under http://$(hostname -I | sed 's/ *$//g'). The files are located in /var/www."
