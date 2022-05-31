ba16b6837b5e8ec0d5cf4f075a274d1c6d811436
Lukas Tautz <104827162+lukastautz@users.noreply.github.com>
2022-05-31 19:27:58 +0200

    Update all.sh

--- all.sh
+++ all.sh
@@ -590,13 +590,13 @@ sudo service nginx restart
 sudo service php8.0-fpm restart
 sudo service php8.1-fpm restart
 sudo service php7.4-fpm restart
-sudo chown -R -v sitemngr /var
-sudo chown -R -v root /var/lib/sudo
-sudo chown -R -v sitemngr /etc/php
-sudo chown -R -v sitemngr /etc/nginx
-sudo chmod -R -v 700 /var
-sudo chmod -R -v 700 /etc/php
-sudo chmod -R -v 700 /etc/nginx
+sudo chown -R sitemngr /var
+sudo chown -R root /var/lib/sudo
+sudo chown -R sitemngr /etc/php
+sudo chown -R sitemngr /etc/nginx
+sudo chmod -R 700 /var
+sudo chmod -R 700 /etc/php
+sudo chmod -R 700 /etc/nginx
 printf "\n"
 printf "\n"
 echo "NGINX Web Server and PHP 7.4/8.0/8.1 are succesfully installed!"
