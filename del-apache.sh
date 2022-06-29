e7c8669e1e2840bbfd28d10b2dd5e001b291d64b
Lukas Tautz <104827162+lukastautz@users.noreply.github.com>
2022-06-29 17:46:05 +0200

    Create del-apache.sh

---
+++ del-apache.sh
@@ -0,0 +1,15 @@
+sudo service apache2 stop
+sudo apt-get purge apache2-bin apache2-data apache2-utils apache2
+sudo apt-get autoremove
+sudo rm -R -v /usr/sbin/apache2
+sudo rm -R -v /usr/sbin/apache2ctl
+sudo rm -R -v /usr/lib/apache2
+sudo rm -R -v /usr/share/apache2
+sudo rm -v /usr/share/man/man8/apache2.8.gz
+sudo rm -R -v /var/lib/apache2
+sudo rm -R -v /run/apache2
+sudo rm -R -v /var/cache/apache2
+sudo rm -R -v /var/lib/apache2
+sudo rm -R -v /var/lock/apache2
+sudo rm -R -v /var/log/apache2
+sudo rm -R -v /var/run/apache2
