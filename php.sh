6e31610f33d38f74e4735d0bdd4b72bec37595d8
Lukas Tautz <104827162+lukastautz@users.noreply.github.com>
2022-07-01 15:17:52 +0200

    Create php.sh

---
+++ php.sh
@@ -0,0 +1,5 @@
+sudo apt install -y apt-transport-https software-properties-common
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+sudo apt update -y
+sudo apt upgrade -y
